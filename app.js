644e50c9f37ad2d9892d662864c87e251543ff1b
ColtonOsterlund <coltonericosterlund@gmail.com>
2021-08-31 19:00:38 -0600

    increasing post request size limit (hopefully)

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const validatePhoneNumber = require('validate-phone-number-node-js');
 const { Console } = require('console');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 app.use(morgan('short'));
 dotenv.config()
 app.use(cors());

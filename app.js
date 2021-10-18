726c69983b1869f2a8d04b65d0b230f32f34597a
ColtonOsterlund <coltonericosterlund@gmail.com>
2021-10-18 15:39:35 -0600

    debugging

--- app.js
+++ app.js
@@ -430,6 +430,42 @@ app.get("/user-test-app", authorizeUser, (req, res) => {
 			}
 		})
 	}
+	else if(req.query.userID != null && req.query.herdID != null && req.query.startDate != null && req.query.endDate != null){
+
+		console.log("\n\n\n\nSTART DATE AND END DATE\n\n\n\n\n");
+		// sqlQuery("SELECT * FROM test WHERE userID = ? AND herdID = ? AND cowID = ?", [encrypt(req.query.userID), encrypt(req.query.herdID), encrypt(req.query.cowID)], (err, objects) => {
+		// 	if(err){
+		// 		return res.send("Error: " + err)
+		// 	}
+		// 	else{
+		// 		var jsonObjects = [] //empty array to put all herds into to then be turned to a JSON object
+
+		// 		objects.forEach(function(test){
+		// 			if(test.userID.substring(0, 11) != "depreciated"){
+
+		// 				var testObject = {
+		// 					objectType: "Test",
+		// 					date: decrypt(test.date),
+		// 					followUpNum: decrypt(test.followUpNum),
+		// 					testID: decrypt(test.testID),
+		// 					testType: decrypt(test.testType),
+		// 					units: decrypt(test.units),
+		// 					value: decrypt(test.value),
+		// 					milivolts: test.milivolts,
+		// 					milkFever: decrypt(test.milkFever),
+		// 					cowID: decrypt(test.cowID),
+		// 					herdID: decrypt(test.herdID),
+		// 					deleted: test.deleted
+		// 				}
+
+		// 				jsonObjects.push(testObject)
+		// 			}
+		// 		})
+
+		// 		return res.send(JSON.stringify(jsonObjects))
+		// 	}
+		// })
+	}
 })
 
 

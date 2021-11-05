9937ff270220fefd1aad8440e8190a7e2d0ad60b
David Herczeg <david.herczeg@live.ca>
2021-11-04 19:01:43 -0600

    Queries for read, update, and delete cow

--- database/cows/cows.js
+++ database/cows/cows.js
@@ -40,6 +40,43 @@ module.exports = {
       return [];
     }
   },
+  readCowById: async (id, userId) => {
+    try {
+      const results = await db.query(escape`
+        SELECT
+          BIN_TO_UUID(id) AS id,
+          cow_id AS cowId,
+          days_in_milk AS daysInMilk,
+          dry_off_day AS dryOffDay,
+          mastitis_history AS mastitisHistory,
+          method_of_dry_off AS methodOfDryOff,
+          daily_milk_average AS dailyMilkAverage,
+          parity,
+          reproduction_status AS reproductionStatus,
+          number_of_times_bred AS numberOfTimesBred,
+          farm_breeding_index AS farmBreedingIndex,
+          lactation_number AS lactationNumber,
+          days_carried_calf_if_pregnant AS daysCarriedCalfIfPregnant,
+          projected_due_date AS projectedDueDate,
+          current_305_day_milk AS current305DayMilk,
+          current_somatic_cell_count AS currentSomaticCellCount,
+          linear_score_at_last_test AS linearScoreAtLastTest,
+          date_of_last_clinical_mastitis AS dateOfLastClinicalMastitis,
+          chain_visible_id AS chainVisibleId,
+          animal_registration_no_nlid AS animalRegistrationNoNlid,
+          dam_breed AS damBreed,
+          culled,
+          BIN_TO_UUID(herd_id) AS herdId
+        FROM cows
+        WHERE id = UUID_TO_BIN(${id})
+        AND user_id = UUID_TO_BIN(${userId})
+      `);
+
+      return results[0] ?? {};
+    } catch (error) {
+      return {};
+    }
+  },
   createCow: async (cow, herdId, userId) => {
     try {
       const result = await db.query(escape`
@@ -98,6 +135,55 @@ module.exports = {
         )
       `);
 
+      return result;
+    } catch (error) {
+      // TODO: Error handling
+      return;
+    }
+  },
+  updateCow: async (cow, id, userId) => {
+    try {
+      const result = await db.query(escape`
+        UPDATE cows
+        SET
+          cow_id = ${cow.cowId},
+          days_in_milk = ${cow.daysInMilk},
+          dry_off_day = ${cow.dryOffDay},
+          mastitis_history = ${cow.mastitisHistory},
+          method_of_dry_off = ${cow.methodOfDryOff},
+          daily_milk_average = ${cow.dailyMilkAverage},
+          parity = ${cow.parity},
+          reproduction_status = ${cow.reproductionStatus},
+          number_of_times_bred = ${cow.numberOfTimesBred},
+          farm_breeding_index = ${cow.farmBreedingIndex},
+          lactation_number = ${cow.lactationNumber},
+          days_carried_calf_if_pregnant = ${cow.daysCarriedCalfIfPregnant},
+          projected_due_date = ${cow.projectedDueDate},
+          current_305_day_milk = ${cow.current305DayMilk},
+          current_somatic_cell_count = ${cow.currentSomaticCellCount},
+          linear_score_at_last_test = ${cow.linearScoreAtLastTest},
+          date_of_last_clinical_mastitis = ${cow.dateOfLastClinicalMastitis},
+          chain_visible_id = ${cow.chainVisibleId},
+          animal_registration_no_nlid = ${cow.animalRegistrationNoNlid},
+          dam_breed = ${cow.damBreed},
+          culled = ${cow.culled},
+          modify_date = ${cow.modifyDate}
+        WHERE id = UUID_TO_BIN(${id})
+        AND user_id = UUID_TO_BIN(${userId})
+      `);
+      return result;
+    } catch (error) {
+      // TODO: Error handling
+      return;
+    }
+  },
+  deleteCow: async (id, userId) => {
+    try {
+      const result = await db.query(escape`
+        DELETE FROM cows
+        WHERE id = UUID_TO_BIN(${id})
+        AND user_id = UUID_TO_BIN(${userId})
+      `);
       return result;
     } catch (error) {
       // TODO: Error handling
--- routes/api/cows.js
+++ routes/api/cows.js
@@ -1,34 +1,90 @@
 const express = require('express');
+const {
+  readCowById,
+  deleteCow,
+  updateCow,
+} = require('../../database/cows/cows');
+const { authenticateToken } = require('../../middleware/auth');
+const { mySqlDateTimeNow } = require('../../utils/format_date');
 
 const router = express.Router();
 
-// TODO: Add middleware for authentication
-
-router.get('/:cowId', async (req, res) => {
-  // TODO: Read specific cow from authenticated user
-  // {req.params.cowId} to get cow ID
-  res.status(200).json({});
+router.get('/:cowId', authenticateToken, async (req, res) => {
+  const cow = await readCowById(req.params.cowId, req.user.id);
+  res.status(200).json(cow);
 });
 
-router.put('/:cowId', async (req, res) => {
-  // TODO: Update specific cow by authenticated user
-  // {req.params.cowId} to get cow ID
-  res.status(201).json({});
+router.put('/:cowId', authenticateToken, async (req, res) => {
+  if (
+    !req.body?.cowId ||
+    !req.body?.daysInMilk ||
+    !req.body?.dryOffDay ||
+    !req.body?.mastitisHistory ||
+    !req.body?.methodOfDryOff ||
+    !req.body?.dailyMilkAverage ||
+    !req.body?.parity ||
+    !req.body?.reproductionStatus ||
+    !req.body?.numberOfTimesBred ||
+    !req.body?.farmBreedingIndex ||
+    !req.body?.lactationNumber ||
+    !req.body?.daysCarriedCalfIfPregnant ||
+    !req.body?.projectedDueDate ||
+    !req.body?.current305DayMilk ||
+    !req.body?.currentSomaticCellCount ||
+    !req.body?.linearScoreAtLastTest ||
+    !req.body?.dateOfLastClinicalMastitis ||
+    !req.body?.chainVisibleId ||
+    !req.body?.animalRegistrationNoNlid ||
+    !req.body?.damBreed ||
+    !req.body?.culled
+  ) {
+    return res.status(400).json({ message: 'Missing fields in request body' });
+  }
+
+  const cow = {
+    cowId: req.body.cowId,
+    daysInMilk: req.body.daysInMilk,
+    dryOffDay: req.body.dryOffDay,
+    mastitisHistory: req.body.mastitisHistory,
+    methodOfDryOff: req.body.methodOfDryOff,
+    dailyMilkAverage: req.body.dailyMilkAverage,
+    parity: req.body.parity,
+    reproductionStatus: req.body.reproductionStatus,
+    numberOfTimesBred: req.body.numberOfTimesBred,
+    farmBreedingIndex: req.body.farmBreedingIndex,
+    lactationNumber: req.body.lactationNumber,
+    daysCarriedCalfIfPregnant: req.body.daysCarriedCalfIfPregnant,
+    projectedDueDate: req.body.projectedDueDate,
+    current305DayMilk: req.body.current305DayMilk,
+    currentSomaticCellCount: req.body.currentSomaticCellCount,
+    linearScoreAtLastTest: req.body.linearScoreAtLastTest,
+    dateOfLastClinicalMastitis: req.body.dateOfLastClinicalMastitis,
+    chainVisibleId: req.body.chainVisibleId,
+    animalRegistrationNoNlid: req.body.animalRegistrationNoNlid,
+    damBreed: req.body.damBreed,
+    culled: req.body.culled,
+    modifyDate: mySqlDateTimeNow(),
+  };
+
+  // TODO: Check if there was an error in updating
+  const result = await updateCow(cow, req.params.cowId, req.user.id);
+
+  res.status(201).json({ message: 'Successfully updated cow' });
 });
 
-router.delete('/:cowId', async (req, res) => {
-  // TODO: Delete specific cow by authenticated user
-  // {req.params.cowId} to get cow ID
-  res.status(201).json({});
+router.delete('/:cowId', authenticateToken, async (req, res) => {
+  // TODO: Check if there was an error in deleting
+  const result = await deleteCow(req.params.cowId, req.user.id);
+  res.status(201).json({ message: 'Successfully deleted cow' });
 });
 
-router.get('/:cowId/strip-tests', async (req, res) => {
+router.get('/:cowId/strip-tests', authenticateToken, async (req, res) => {
   // TODO: Read all strip tests of a cow from authenticated user
   // {req.params.cowId} to get cow ID
   res.status(200).json({});
 });
 
-router.post('/:cowId/strip-tests', async (req, res) => {
+router.post('/:cowId/strip-tests', authenticateToken, async (req, res) => {
   // TODO: Create new strip test in a cow by authenticated user
   // {req.params.cowId} to get cow ID
   res.status(201).json({});
